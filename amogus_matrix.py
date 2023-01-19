014b5d30b7dd6f3cf5c21ee6f5fa901cbb030a85
TheBenonator <76881272+LustigerNutzer@users.noreply.github.com>
2023-01-19 10:23:27 +0000

    Add files via upload

---
+++ amogus_matrix.py
@@ -0,0 +1,207 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+R = [255,0,0]
+C = [0,219,219]
+B = [0,0,0]
+O = [255,255,255]
+
+amogus1 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,B,
+O,O,O,O,O,O,O,C,
+O,O,O,O,O,O,O,B,
+O,O,O,O,O,O,O,B,
+O,O,O,O,O,O,O,B,
+O,O,O,O,O,O,O,O,
+]
+
+amogus2 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,B,
+O,O,O,O,O,O,B,R,
+O,O,O,O,O,O,C,C,
+O,O,O,O,O,O,B,R,
+O,O,O,O,O,O,B,R,
+O,O,O,O,O,O,B,R,
+O,O,O,O,O,O,O,B,
+]
+
+amogus3 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,B,B,
+O,O,O,O,O,B,R,R,
+O,O,O,O,O,C,C,R,
+O,O,O,O,O,B,R,R,
+O,O,O,O,O,B,R,R,
+O,O,O,O,O,B,R,B,
+O,O,O,O,O,O,B,O,
+]
+
+amogus4 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,B,B,B,
+O,O,O,O,B,R,R,R,
+O,O,O,O,C,C,R,R,
+O,O,O,O,B,R,R,R,
+O,O,O,O,B,R,R,R,
+O,O,O,O,B,R,B,B,
+O,O,O,O,O,B,O,O
+]
+
+amogus5 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,B,B,B,B,
+O,O,O,B,R,R,R,R,
+O,O,O,C,C,R,R,R,
+O,O,O,B,R,R,R,R,
+O,O,O,B,R,R,R,R,
+O,O,O,B,R,B,B,R,
+O,O,O,O,B,O,O,B
+]
+
+amogus6 = [
+O,O,O,O,O,O,O,O,
+O,O,O,B,B,B,B,O,
+O,O,B,R,R,R,R,B,
+O,O,C,C,R,R,R,B,
+O,O,B,R,R,R,R,B,
+O,O,B,R,R,R,R,B,
+O,O,B,R,B,B,R,B,
+O,O,O,B,O,O,B,O
+]
+
+amogus7 = [
+O,O,O,O,O,O,O,O,
+O,O,B,B,B,B,O,O,
+O,B,R,R,R,R,B,O,
+O,C,C,R,R,R,B,R,
+O,B,R,R,R,R,B,R,
+O,B,R,R,R,R,B,R,
+O,B,R,B,B,R,B,O,
+O,O,B,O,O,B,O,O
+]
+
+amogus8 = [
+O,O,O,O,O,O,O,O,
+O,B,B,B,B,O,O,O,
+B,R,R,R,R,B,O,O,
+C,C,R,R,R,B,R,O,
+B,R,R,R,R,B,R,O,
+B,R,R,R,R,B,R,O,
+B,R,B,B,R,B,O,O,
+O,B,O,O,B,O,O,O
+]
+
+amogus9 = [
+O,O,O,O,O,O,O,O,
+B,B,B,B,O,O,O,O,
+R,R,R,R,B,O,O,O,
+C,R,R,R,B,R,O,O,
+R,R,R,R,B,R,O,O,
+R,R,R,R,B,R,O,O,
+R,B,B,R,B,O,O,O,
+B,O,O,B,O,O,O,O
+]
+
+amogus10 = [
+O,O,O,O,O,O,O,O,
+B,B,B,O,O,O,O,O,
+R,R,R,B,O,O,O,O,
+R,R,R,B,R,O,O,O,
+R,R,R,B,R,O,O,O,
+R,R,R,B,R,O,O,O,
+B,B,R,B,O,O,O,O,
+O,O,B,O,O,O,O,O
+]
+
+amogus11 = [
+O,O,O,O,O,O,O,O,
+B,B,O,O,O,O,O,O,
+R,R,B,O,O,O,O,O,
+R,R,B,R,O,O,O,O,
+R,R,B,R,O,O,O,O,
+R,R,B,R,O,O,O,O,
+B,R,B,O,O,O,O,O,
+O,B,O,O,O,O,O,O
+]
+
+amogus12 = [
+O,O,O,O,O,O,O,O,
+B,O,O,O,O,O,O,O,
+R,B,O,O,O,O,O,O,
+R,B,R,O,O,O,O,O,
+R,B,R,O,O,O,O,O,
+R,B,R,O,O,O,O,O,
+R,B,O,O,O,O,O,O,
+B,O,O,O,O,O,O,O
+]
+
+amogus13 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+B,O,O,O,O,O,O,O,
+B,R,O,O,O,O,O,O,
+B,R,O,O,O,O,O,O,
+B,R,O,O,O,O,O,O,
+B,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O
+]
+
+amogus14 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+R,O,O,O,O,O,O,O,
+R,O,O,O,O,O,O,O,
+R,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O
+]
+
+amogus15 = [
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O,
+O,O,O,O,O,O,O,O
+]
+
+while True:
+	sense.set_pixels(amogus1)
+	sleep(.1)
+	sense.set_pixels(amogus2)
+	sleep(.1)
+	sense.set_pixels(amogus3)
+	sleep(.1)
+	sense.set_pixels(amogus4)
+	sleep(.1)
+	sense.set_pixels(amogus5)
+	sleep(.1)
+	sense.set_pixels(amogus6)
+	sleep(.1)
+	sense.set_pixels(amogus7)
+	sleep(.1)
+	sense.set_pixels(amogus8)
+	sleep(.1)
+	sense.set_pixels(amogus9)
+	sleep(.1)
+	sense.set_pixels(amogus10)
+	sleep(.1)
+	sense.set_pixels(amogus11)
+	sleep(.1)
+	sense.set_pixels(amogus12)
+	sleep(.1)
+	sense.set_pixels(amogus13)
+	sleep(.1)
+	sense.set_pixels(amogus14)
+	sleep(.1)
+	sense.set_pixels(amogus15)
+	sleep(.1)
+
